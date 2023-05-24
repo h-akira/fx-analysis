159cdac5ba7bcd60cd409cc51d1545ff8131e5a4
h-akira <79441679+h-akira@users.noreply.github.com>
2023-05-24 09:27:12 +0900

    success fill_between

--- src/test.py
+++ src/test.py
@@ -8,6 +8,7 @@ import sys
 import os
 import pandas as pd
 import mplfinance as mpf
+import datetime
 # import matplotlib.pyplot as plt
 
 def parse_args():
@@ -27,9 +28,27 @@ def main():
   df = pd.read_csv(options.file, encoding='shift_jis').rename(columns={'日時':'date', '始値(BID)':'Open', '高値(BID)':'High', '安値(BID)':'Low', '終値(BID)':'Close'})
   df["date"] = pd.to_datetime(df["date"])
   df.set_index("date", inplace=True)
+  df = df.head(100)
   print(df)
-  mpf.plot(df.head(100), type="candle", mav=[20], hlines={'hlines':[136.28,136.32],'colors':['g','r']},
-      vlines={'vlines':['2023-05-01 07:23','2023-05-01 07:43'],'colors':['g','r']})
+  dates_df = pd.DataFrame(df.index)
+  buy_time=pd.Timestamp('2023-05-01 07:23'),
+  sell_time=pd.Timestamp('2023-05-01 07:33')
+  y1value = df['Close'].max()
+  y2value = df['Low'].min()
+  where_values = pd.notnull(dates_df[(dates_df>=buy_time)&(dates_df<=sell_time)])['date'].values
+  print(where_values)
+  mpf.plot(df, type="candle", mav=[20], hlines={'hlines':[136.28,136.32],'colors':['g','r']},
+      vlines={'vlines':['2023-05-01 07:23','2023-05-01 07:43'],'colors':['g','r']},
+      fill_between=dict(y1=y1value, y2=y2value, where=where_values))
+      # fill_between=dict(
+      # x1=pd.Timestamp('2023-05-01 07:23'),
+      # x2=pd.Timestamp('2023-05-01 07:23')
+      # x1=10,
+      # x2=20)
+      # x1=datetime.datetime.strptime('2023-05-01 07:23','%Y-%m-%d %H:%M'),
+      # x2=datetime.datetime.strptime('2023-05-01 07:23','%Y-%m-%d %H:%M')
+  # fill_between=dict(x1='2023-05-01 07:23',x2='2023-05-01 07:43'))
+               #x1='2023-05-01 07:23',x2='2023-05-01 07:43'))
 
 if __name__ == '__main__':
   main()
